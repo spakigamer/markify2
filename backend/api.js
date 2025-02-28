6529805a365bde3e7d8328a434f19dcb85c593b9
Dhruv <dhruvgoel165@gmail.com>
2025-02-28 14:22:53 +0530

    aad

--- backend/api.js
+++ backend/api.js
@@ -41,7 +41,7 @@ const Note = mongoose.model('Note', noteSchema);
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000','https://markify2.vercel.app'],
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
