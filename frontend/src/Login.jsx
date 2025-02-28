85a50d7809479d3bea6d3b49a6787851ae0ca02a
Dhruv <dhruvgoel165@gmail.com>
2025-02-28 16:24:44 +0530

    parsing

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -17,7 +17,6 @@ function Login() {
 
     const onSubmit = async (data) => {
         try {
-            data=JSON.parse(data)
             const response = await axios.post("https://markify2.onrender.com/login", data, {
                 withCredentials: true, // Allows cookies to be sent
             });
